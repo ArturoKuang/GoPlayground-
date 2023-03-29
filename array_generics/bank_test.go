6439bfd78dfb74152540af936839f964c917602e
Arturo Kuang <akuang@wyze.com>
2023-03-28 22:02:10 -0700

    arrays with generics

---
+++ array_generics/bank_test.go
@@ -0,0 +1,60 @@
+package arraygenerics
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestBadBank(t *testing.T) {
+	var (
+		riya  = Account{Name: "Riya", Balance: 100}
+		chris = Account{Name: "Chris", Balance: 75}
+		adil  = Account{Name: "Adil", Balance: 200}
+
+		transactions = []Transaction{
+			NewTransaction(chris, riya, 100),
+			NewTransaction(adil, chris, 25),
+		}
+	)
+
+	newBalanceFor := func(account Account) float64 {
+		return NewBalanceFor(account, transactions).Balance
+	}
+
+	AssertEqual(t, newBalanceFor(riya), 200)
+	AssertEqual(t, newBalanceFor(chris), 0)
+	AssertEqual(t, newBalanceFor(adil), 175)
+}
+
+type Person struct {
+	Name string
+}
+
+func TestFind(t *testing.T) {
+	t.Run("Find the best programmer", func(t *testing.T) {
+		people := []Person{
+			Person{Name: "Kent Beck"},
+			Person{Name: "Martin Fowler"},
+			Person{Name: "Chris James"},
+		}
+
+		king, found := Find(people, func(p Person) bool {
+			return strings.Contains(p.Name, "Chris")
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, king, Person{Name: "Chris James"})
+	})
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	if got != want {
+		t.Errorf("got: %v want: %v", got, want)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	if !got {
+		t.Errorf("got: %v want: %v", got, true)
+	}
+}
